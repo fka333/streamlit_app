da219591f3cc113a2fcb7c4f47d4260a3e045d9f
fka333 <44562517+fka333@users.noreply.github.com>
2022-05-27 14:40:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,12 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas as pd
 
 data = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-streamlit.dataframe(data)
 
-selections = streamlit.multiselect('Pick some fruits: ', list(data.index))
+# create item picker
+# make pre-selection with Avocado and Strawberries
+selections = streamlit.multiselect('Pick some fruits: ', list(data.index), ['Avocado', 'Strawberries'])
+fruit_to_show = data.loc[selections]
 streamlit.text(selections)
+
+# display dataframe of fruit macro
+streamlit.dataframe(fruit_to_show)
